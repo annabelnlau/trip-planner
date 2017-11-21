1d5260bbacdd567cdb19b7f9c4d036cd12e40f9e
Annabel Lau <annabel.n.lau@gmail.com>
2017-11-21 11:45:15 -0500

    got marker working

--- public/bundle.js
+++ public/bundle.js
@@ -74,10 +74,16 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiamFjcXVlbHlud2F4IiwiYSI6ImNqYTl0cDc5aDBsdjkyc
 const map = new mapboxgl.Map({
   container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
+  zoom: 15, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 })
 
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
 
 /***/ }),
 /* 1 */
